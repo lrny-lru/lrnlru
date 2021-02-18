66a59bf675eeede5f447869288ace8b49ffd0ba3
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2021-02-18 18:13:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+     
+        <h1 class="myName">Lauren Larue Joyner</h1>
+        <Header />
+     
+       
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <div>
+      
+      
+      <nav>
+        <ul>
+          <li><a href="#home">info</a></li>
+          <li><a href="#about">about</a></li>
+          <li><a href="#projects">projects</a></li>
+          <li><a href="#services">services</a></li>
+          <li><a href='#message'>message me</a></li>
+           <li><a href="#links">links</a></li>
+        </ul>
+     
+         </nav>
+    </div>
+
+  );
+}
+
+export default Header;
